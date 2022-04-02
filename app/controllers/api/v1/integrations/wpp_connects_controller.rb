de6c80efd34bdd9d77192c58178c8f229c06694a
Douglas Lara <douglasdelaras@gmail.com>
2022-04-02 17:58:01 -0300

    Fix messages duplication

--- app/controllers/api/v1/integrations/wpp_connects_controller.rb
+++ app/controllers/api/v1/integrations/wpp_connects_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Integrations::WppConnectsController < ApplicationController
   def webhook
-    Integrations::WppConnects::EventsJob.perform_later(params.to_json)
+    Integrations::WppConnects::EventsJob.set(wait: 1.second).perform_later(params.to_json)
     render json: { ok: 'success' }, status: :ok
   end
 end
